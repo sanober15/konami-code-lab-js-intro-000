2ac67b6cf71f6e80126fe5308ec18c24abaa59ce
sanober riaz <sanober@sanober.me>
2018-01-09 20:41:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,13 +3,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let i = 0
-  const s = document.querySelectorAll('body')
-  for (var i = 0; i < code.length; i++) {
-    let code[i] = parseInt(input.which || input.detail);
-    input.addEventListener('keydown', function(e){
-      if (e.which === input) {
+  document.body.addEventListener('keydown', function(e){
+    const input = parseInt(e.which || e.detail);
+
+    if (code[i] === input) {
+      i++;
+      if (i === code.length) {
         alert ("Hurray!");
       }
-    })
-  }
+    }else {
+      index = 0;
+    }
+  })
 }
